aef77fbdb3070c9d6380ee135e80c011fd504987
Khabert <khabertzion11@gmail.com>
2023-07-31 11:11:50 -0700

    changes in api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,50 @@
 #!/usr/bin/python3
-""" API v1 """
+'''Contains a Flask web application API.
+'''
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
-import os
+
+
 app = Flask(__name__)
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
-def close(close):
-    """ Close the session """
+def teardown_flask(exception):
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
     storage.close()
 
 
 @app.errorhandler(404)
-def error_handler(error):
-    """ Error Handler """
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
 
-if __name__ == "__main__":
-    h = os.getenv('HBNB_API_HOST') if os.getenv('HBNB_API_HOST') else '0.0.0.0'
-    p = os.getenv('HBNB_API_PORT') if os.getenv('HBNB_API_PORT') else 5000
-    app.run(host=h, port=p)
-    threaded = True
+
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
+
+
+if __name__ == '__main__':
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-""" index """
-from api.v1.views import app_views
+'''Contains the index view for the API.'''
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def route():
-    """ Return a Json response """
-    return jsonify({"status": "OK"})
+def status():
+    """ Returns JSON """
+    return jsonify(status="OK")
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def some_stats():
-    """Retrieves the number of each objects by type"""
-    stats_dic = {"amenities": storage.count("Amenity"),
-                 "cities": storage.count("City"),
-                 "places": storage.count("Place"),
-                 "reviews": storage.count("Review"),
-                 "states": storage.count("State"),
-                 "users": storage.count("User")}
-    return jsonify(stats_dic)
-
-
-if __name__ == "__main__":
-    pass
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
